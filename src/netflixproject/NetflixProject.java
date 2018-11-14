ccbd69a2f21265dae94d2f2c7abd84e1a5b9b26f
shabs <43846967+shabs82@users.noreply.github.com>
2018-11-14 12:45:35 +0100

    system print hello

--- src/netflixproject/NetflixProject.java
+++ src/netflixproject/NetflixProject.java
@@ -22,7 +22,7 @@ public class NetflixProject extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        System.out.println(" hello" );
         stage.setScene(scene);
         stage.show();
     }
